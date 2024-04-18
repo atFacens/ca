87deb8086a84bb471684f42b5abe384a4c1fa2b8
EmersonPaduan <emerson.paduan@gmail.com>
2024-04-17 22:02:20 -0300

    aula 09

---
+++ aula09/exemplo01.py
@@ -0,0 +1,8 @@
+
+condicao = True
+
+while condicao : # while = Enquanto ou seja, enquanto a condição for verdadeira, continua repetindo
+    print('valor', 1)
+    condicao = False
+
+print('outra coisa')
