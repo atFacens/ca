09a5a5cd301d099d9aca6018ed0e2eb8c682db15
EmersonPaduan <emerson.paduan@gmail.com>
2024-08-31 00:35:01 -0300

    aula 02

---
+++ aula02/exemplo01.py
@@ -0,0 +1,16 @@
+# Esta linha é um comentário
+
+# O comando print escreve uma mensagem na tela
+print("Boa noite!")
+
+# Aqui o print faz a soma antes de exibir o resultado
+print(12 + 5)
+
+print("12 + 5")
+
+print(1, 2, 3)
+
+# sep = separador, end = terminador
+print(1, 2, 3, sep=", ", end=".\n")
+
+print("final")
---
+++ aula02/exemplo02.py
@@ -0,0 +1,19 @@
+# variável é uma área de memória para guardar informações
+
+# cria uma variável e armazena um número (int)
+numero = 12
+
+print(numero)
+
+print(type(numero)) # int
+
+# armazena um texto (string) na variável
+numero = "Um texto"
+
+print(numero)
+
+print(type(numero)) # str
+
+# Boa prática: use nomes significativos para as variáveis
+nameUser = "Emerson"
+emailUser =  "emerson@email.com"
